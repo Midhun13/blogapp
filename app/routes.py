c412ac2d76836b85a4d4d9bfd01ca9073b7adf87
Midhun13 <midhunbms568@gmail.com>
2024-12-27 13:05:00 +0530

    first commit

--- app/routes.py
+++ app/routes.py
@@ -10,4 +10,13 @@ from wtforms.validators import DataRequired
 @app.route('/index')
 def index():
     return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
