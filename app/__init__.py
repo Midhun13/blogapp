c57dd0af3bc3ada3325609c6a97d78d778e9209d
Midhun13 <midhunbms568@gmail.com>
2025-01-06 10:51:59 +0530

    modified files added

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
 app = Flask(__name__)
-from app import routes
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:IPCSATTINGAL@localhost/blog'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
+
+from app import routes, models
